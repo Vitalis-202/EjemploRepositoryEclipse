c5a7d838ebc006c9c2f862be525836e8b2466524
Pablo Perez Lee <pablo.perez.lee@gmail.com>
2021-08-26 18:59:57 -0400

    Inicia el proyecto

---
+++ PracticaPrueba1-1/src/main/java/com/duoc/springjpa/PracticaPrueba11Application.java
@@ -0,0 +1,13 @@
+package com.duoc.springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaPrueba11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaPrueba11Application.class, args);
+	}
+
+}
